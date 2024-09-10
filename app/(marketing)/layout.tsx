c13aff42af6c5a31dedc3d84e17ddcd78cad08c3
Dinnos0507 <dinnos0507@gmail.com>
2024-09-10 23:38:30 +0700

    feat: init project

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,16 @@
+type Props = {
+    children:React.ReactNode;
+};
+
+const MarketingLayout =({ children }: Props) => {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <main className="flex-1
+            flex flex-auto flex-col last:items-center justify-items-center">
+                {children}
+            </main>
+        </div>
+    )
+};
+
+export default MarketingLayout;
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button"
+export default function Home() {
+  return (
+    <div>
+        This is a page LOL:
+    </div>
+  )
+}
