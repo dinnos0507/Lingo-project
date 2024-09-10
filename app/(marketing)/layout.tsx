5c7158fd09c7196aba6af5dec3759256c93d5b21
Dinnos0507 <dinnos0507@gmail.com>
2024-09-10 23:56:04 +0700

    fix: remove unused button

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,12 +1,12 @@
 type Props = {
-    children:React.ReactNode;
-};
+    children: React.ReactNode;
+}
 
-const MarketingLayout =({ children }: Props) => {
+const MarketingLayout = ({ children }: Props) => {
     return (
         <div className="min-h-screen flex flex-col">
             <main className="flex-1
-            flex flex-auto flex-col last:items-center justify-items-center">
+            flex flex-col  last:items-center justify-items-center">
                 {children}
             </main>
         </div>
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <div>
-        This is a page LOL:
+      <Button>Wau</Button>
+      This is  a page LOL:
     </div>
   )
 }
