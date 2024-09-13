ebc4d684e73525aa1f5c9b46a414a867482372e7
Dinnos0507 <dinnos0507@gmail.com>
2024-09-13 10:02:50 +0700

    Add something in learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,24 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper"
+import { UserProgress } from "@/components/user-progress";
+
+import { Header } from "./header";
+
 const LearnPage = () => {
     return (
-        <div>
-            Learn Page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgress
+                    activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Spanish" />
+
+            </FeedWrapper>
         </div>
     )
 }
