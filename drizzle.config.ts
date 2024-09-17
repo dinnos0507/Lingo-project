74040755b6ca3f9f3620a7f43500e803ef0cd014
Dinnos0507 <dinnos0507@gmail.com>
2024-09-18 01:08:20 +0700

    Successful add Drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import "dotenv/config"
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./db/schema.ts", 
+  out: "./drizzle",
+  dbCredentials : 
+  {
+    url: process.env.DATABASE_URL!,
+  },
+}); 
