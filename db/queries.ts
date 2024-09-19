cc9562c2d8606a765dcf7f3f635f5468ebb82d3d
Dinnos0507 <dinnos0507@gmail.com>
2024-09-20 00:06:26 +0700

    Add Lesson Button

--- db/queries.ts
+++ db/queries.ts
@@ -8,6 +8,7 @@ import {
     userProgress,
     units,
     challenges,
+    challengeProgress,
 } from "@/db/schema"
 
 
@@ -28,12 +29,14 @@ export const getUserProgress = cache(async () =>{
 });
 
 export const getUnits = cache(async () => {
+    const { userId } = await auth();
     const userProgress = await getUserProgress();
 
-    if(!userProgress?.activeCourseId) {
+    if(!userId || !userProgress?.activeCourseId) {
         return [];
     }
 
+    // TODO: Confirm whether order is needed
     const data = await db.query.units.findMany({
        where: eq(units.courseId, userProgress.activeCourseId),
        with: {
@@ -41,7 +44,12 @@ export const getUnits = cache(async () => {
                 with: {
                     challenges: {
                         with: {
-                            challengeProgress: true,
+                            challengeProgress:{
+                                where : eq(
+                                    challengeProgress.userId,
+                                    userId,
+                                )
+                            }
                         }
                     }
                 }
