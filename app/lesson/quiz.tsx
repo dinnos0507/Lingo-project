727095ea585f601524234b982bc5eb9128e90afc
Dinnos0507 <dinnos0507@gmail.com>
2024-09-23 12:05:11 +0700

    Challenge Actions

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,13 +1,16 @@
 "use client"
 
-import { useState } from "react";
+import { useState, useTransition } from "react";
+import { toast } from "sonner";
 
 import {challengeOptions, challenges} from "@/db/schema"
+import { upsertChallengeProgress } from "@/actions/challenge-progress";
 
 import { Header } from "./header";
 import { Footer } from "./footer";
 import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
+import { reduceHearts } from "@/actions/user-progress";
 
 type Props = {
     initialPercentage:number;
@@ -27,8 +30,10 @@ export const Quiz = ({
     initialLessonChallenges,
     userSubscription,
 }:Props)  => {
+    const [pending, startTransition] = useTransition();
+
     const [hearts, setHearts] = useState(initialHearts);
-    const [percentage, setpercentage] = useState(initialPercentage);
+    const [percentage, setPercentage] = useState(initialPercentage);
     const [challenges] = useState(initialLessonChallenges);
     const [activeIndex, setActiveIndex] = useState (()=> {
         const uncompletedIndex = challenges.findIndex((challenge) => !challenge.completed);
@@ -41,12 +46,75 @@ export const Quiz = ({
     const challenge = challenges[activeIndex];
     const options = challenge?.challengeOptions ?? [];  
 
+    const onNext = () =>{
+        setActiveIndex((current) => current + 1)
+    };
+
     const onSelect = (id:number) => {
         if (status !== "none") return;
 
         setSelectedOption (id);
     }
 
+    const onContinue = () => {
+        if(!selectedOption) return;
+
+        if(status === "wrong") {
+            setStatus("none");
+            setSelectedOption(undefined);
+            return;
+        }
+        if(status === "correct") {
+            onNext();
+            setStatus("none");
+            setSelectedOption(undefined);
+            return;
+        }
+
+        const correctOption = options.find((option) => option.correct);
+
+        if (!correctOption) {
+            return;
+        }
+
+        if (correctOption && correctOption.id === selectedOption ) {
+            startTransition(() => {
+                upsertChallengeProgress(challenge.id)
+                    .then((response) => {
+                        if(response?.error === "hearts") {
+                            console.error("Missing hearts") 
+                            return ;
+                        }
+
+                        setStatus("correct");                      
+                        setPercentage((prev) => prev+100/challenges.length);
+                        
+                        // This is practice
+                        if(initialPercentage === 100) {
+                            setHearts((prev) => Math.min(prev +1, 5));
+                        }
+                    })
+                    .catch(() => toast.error("Something went wrong. Please try again."))
+            })
+        } else {
+            startTransition(() => {
+                reduceHearts(challenge.id)
+                    .then((response) => {
+                        if (response?.error === "hearts") {
+                            console.error("Missing hearts");
+                            return;
+                        }
+                    
+                        setStatus("wrong");
+                        
+                        if(!response?.error){
+                            setHearts((prev) => Math.max(prev -1, 0))
+                        }
+                    })
+                    .catch(() => toast.error("Something went wrong. Please try again"))
+            })
+        }
+    }
 
     const title = challenge.type === "ASSIST"
         ? "Select the correct meaning"
@@ -74,7 +142,7 @@ export const Quiz = ({
                                 onSelect={onSelect}
                                 status={status}
                                 selectedOption ={selectedOption}
-                                disabled={false}
+                                disabled={pending}
                                 type={challenge.type}
                             /> 
                         </div>
@@ -82,9 +150,9 @@ export const Quiz = ({
                 </div>
             </div>
             <Footer
-                disabled = {!selectedOption}
+                disabled = {pending || !selectedOption}
                 status={status}
-                onCheck={() => {}}
+                onCheck={onContinue}
             />
         </>
     )
